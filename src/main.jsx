a7ae6a5fbdef88b55b3f43ef762f1ac47cb85b34
Ragul Dravid S <raguldravid917@gmail.com>
2025-12-12 16:17:14 +0530

    Initial commit - final ecommerce app

--- src/main.jsx
+++ src/main.jsx
@@ -5,32 +5,16 @@ import App from "./App.jsx";
 import AuthProvider from "./context/AuthContext.jsx";
 import CartProvider from "./context/CartContext.jsx";
 import "./index.css";
-import { HashRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <CartProvider>
-           <HashRouter>
-             <App />
-           </HashRouter>
+          <App />
         </CartProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
 
-// before:
-// import { BrowserRouter } from "react-router-dom";
-
-// after:
-// import { HashRouter } from "react-router-dom";
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <HashRouter>
-//       <App />
-//     </HashRouter>
-//   </React.StrictMode>
-// );
